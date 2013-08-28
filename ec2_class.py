33777c1e23af3b6b53ca101e70044151643c5001
Chris Kanich <ckanich@uic.edu>
2013-08-27 19:46:38 -0500

    about to start sending creds :O

--- ec2_class.py
+++ ec2_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import boto
+import csv
 from optparse import OptionParser
 import sys
 from time import sleep,time
@@ -8,7 +9,45 @@ from subprocess import Popen, PIPE
 from tempfile import mkdtemp
 from shutil import rmtree
 import shlex
+from random import choice
+import string
 
+import smtplib
+import os
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEBase import MIMEBase
+from email.MIMEText import MIMEText
+from email.Utils import COMMASPACE, formatdate
+from email import Encoders
+
+
+passwordcharacters = string.letters + string.digits
+def generate_password(length=8):
+  return ''.join(choice(passwordcharacters) for _ in xrange(length))
+  
+
+def send_mail(send_from, send_to, send_cc, subject, text, files={}, server="bcuda-east.cc.uic.edu."):
+  assert type(files)==dict
+
+  msg = MIMEMultipart()
+  msg['From'] = send_from
+  msg['To'] = send_to
+  msg['CC'] = send_cc
+  msg['Date'] = formatdate(localtime=True)
+  msg['Subject'] = subject
+
+  msg.attach( MIMEText(text) )
+
+  for fname,fbody in files.items():
+    part = MIMEBase('application', "octet-stream")
+    part.set_payload( fbody )
+    Encoders.encode_base64(part)
+    part.add_header('Content-Disposition', 'attachment; filename="%s"' % fname)
+    msg.attach(part)
+
+  smtp = smtplib.SMTP(server)
+  smtp.sendmail(send_from, send_to, msg.as_string())
+  smtp.close()
 
 iam = boto.connect_iam()
 ec2 = boto.connect_ec2()
@@ -20,8 +59,11 @@ SEMESTER = 'f13'
 AMI = 'ami-d0f89fb9' # for now, standard ubuntu 12.04.2 LTS
 ACCOUNT = '020404094600'
 SAFE = True # in case of errors, bomb out instead of wiping old
+INSTRUCTOR = 'Chris Kanich <ckanich@uicbits.net>' # from address
 
+CC_ADDR = 'Chris Kanich <ckanich@uic.edu>'
 
+SAFE = False
 
 policy_boilerplate = '''
 {
@@ -30,23 +72,43 @@ policy_boilerplate = '''
       "Effect": "Allow",
       "Action": [
         "ec2:StopInstances", 
-        "ec2:StartInstances",
-        "ec2:DescribeInstances"
+        "ec2:StartInstances"
       ],
       "Resource": "arn:aws:ec2:us-east-1:%s:instance/%s"
+    }
+   ],
+   "Statement": [{
+      "Effect": "Allow",
+      "Action": [
+        "ec2:DescribeInstance*"
+      ],
+      "Resource": "*"
     }
    ]
 }
 '''
 
-config_boilerplate = '''
-[Credentials]
-aws_access_key_id = %s
-aws_secret_access_key = %s
-'''
 
-'''
-create keyfiles locally
+email_text = '''
+Your ec2 username is %s and your password is %s. You can use 
+these credentials to log in to the web interface at
+https://ckanich.signin.aws.amazon.com. 
+
+To start your VM:
+
+In the web interface, navigate to Services -> EC2 -> Instances to see the list
+of instances under my account. Your username will be the "Name" of your virtual
+machine. If your virtual machine is shut off due to idleness, you can restart
+it by highlighting it and selecting Actions -> Start Instance. Once it has
+started, the "Public DNS" is the address you should ssh to to use the VM.
+
+To log in to your VM:
+
+id_rsa (attached to this email) is your ssh private key. To use it by default
+in a *nix environment, place it in ~/.ssh/ and make sure that it is unreadable
+by group or other. The username for sshing into your virtual machine is
+'ubuntu'.
+
 '''
 
 def create_local_keypair(username):
@@ -61,6 +123,20 @@ def create_local_keypair(username):
   private_key = open(mydir + '/prefix').read()
   rmtree(mydir)
   return public_key, private_key
+
+class Student:
+  def __init__(self,csvline):
+    for key,value in csvline.items():
+      setattr(self,key,value)
+    self.username = self.email.split('@')[0]
+
+  def emailto(self):
+    return "%s %s <%s>" % (self.firstname, self.lastname, self.email)
+
+  def send_mail(self):
+    assert hasattr(self,'privatekey')
+    files = {'id_rsa':self.privatekey}
+    send_mail(INSTRUCTOR,self.emailto(),CC_ADDR,"Your CS450 VM credentials",email_text % (self.student_id,self.pw),files)
 
 
 
@@ -129,13 +205,17 @@ def wait_for_instance(instance_id,timeout=5*60,retry=5):
         print "instance started."
         return
       print "instance not yet running"
-    except:
+    except Exception,e:
+      print e
       print "instance not visible,retrying"
     sleep(retry)
   print "instance did not come up before timeout. Aborting."
   sys.exit(1)
 
 def create_class(classlist):
+  keydir = "%s-%s" % (CLASS,SEMESTER)
+  os.mkdir(keydir)
+  students = []
   group = create_security_group("%s.%s" % (CLASS, SEMESTER))
   previous_rules = [str(x) for x in group.rules]
   for portnum in (22,80,443):
@@ -145,9 +225,13 @@ def create_class(classlist):
     group.authorize('udp',53,53,'0.0.0.0/0')
 
   for student in classlist:
-    student_id = '%s.%s.%s' % (CLASS,SEMESTER,student)
+    student_id = '%s.%s.%s' % (CLASS,SEMESTER,student.username)
+    student.student_id = student_id
     # create student
     response = create_user(student_id)
+    pw = generate_password()
+    student.pw = pw
+    iam.create_login_profile(student_id,pw)
     print "user created:",repr(response)
     user_id = response.user_id
     response = create_access_key(student_id)
@@ -155,21 +239,34 @@ def create_class(classlist):
     secret_key = response.secret_access_key
     # create student ssh keypair
     public_key,private_key = create_key_pair(student_id)
+    student.public_key = public_key
+    student.private_key = private_key
+    with open(keydir + '/' + student.username + '.pub','w') as f:
+      f.write(public_key)
 
     # create VM
     reservation = ec2.run_instances(AMI,
-                      instance_type='t1.micro',
+                      instance_type='m1.small',
                       security_groups=(group.name,),
                       key_name=student_id
                       )
     print "instance run:",repr(reservation.instances[0])
     instance = reservation.instances[0]
-    instance.add_tag('Name',student_id)
+    attempts = 5
+    while attempts > 0:
+      try:
+        instance.add_tag('Name',student_id)
+        break
+      except e:
+        print e
+        sleep(5)
+        attempts -= 1
+      assert attempts != 0
     instance_id = instance.id
     wait_for_instance(instance_id)
     ec2.stop_instances((instance_id,))
 
-    # allow student to stop and start the instance:
+    # allow student to stop and start and see the instance:
     result = iam.put_user_policy(student_id,student_id,policy_boilerplate.strip() % (ACCOUNT,instance_id))
     print "student policy installed."
 
@@ -178,54 +275,24 @@ def create_class(classlist):
     action='arn:aws:automate:us-east-1:ec2:stop'
     alarm = boto.ec2.cloudwatch.alarm.MetricAlarm(name=student_id,namespace="AWS/EC2",metric="NetworkOut",dimensions={"InstanceId":instance_id}, comparison="<=",threshold=(100*1024),period=60*60,evaluation_periods=6,statistic="Sum",unit="Bytes",alarm_actions=[action,])
     metric = cloudwatch.put_metric_alarm(alarm)
+    
+    student.send_mail()
 
 
-    with os.fdopen(os.open('id_aws', os.O_WRONLY | os.O_CREAT, 0600), 'w') as handle:
-      handle.write(private_key)
-
-    with os.fdopen(os.open(student,'.boto', os.O_WRONLY | os.O_CREAT, 0600), 'w') as handle:
-      handle.write(config_boilerplate % (access_key,secret_key))
-
-    instance_id_file = open('my_vm_id','w')
-    instance_id_file.write(instance_id)
-    instance_id_file.close()
 
 
-def start_student_vm():
-  try:
-    f = open('my_vm_id')
-    my_vm_id = f.read().strip()
-  except:
-    print "couldn't open file my_vm_id in the current directory."
-    sys.exit()
-  ret = start_vm(my_vm_id) 
-  if ret == False:
-    print "could not start your VM."
-    sys.exit(1)
-  wait_for_instance(my_vm_id)
-  # tell student what the VM's IP is
-  reservations = ec2.get_all_instances(instance_ids=(my_vm_id,))
-  print "you can ssh to your VM at %s" % reservations[0].instances[0].public_dns_name
- 
+def read_student_file(filename):
+  students = []
+  with open(filename) as f:
+    for item in csv.DictReader(f):
+      students.append(Student(item))
+  return students
 
+students = read_student_file('cs450.csv')
 
-def start_vm(vm_id):
-  now = time()
-  retry = 5
-  success = False
-  while time() < (now + 5*60):
-    try:
-      # start the VM
-      started = ec2.start_instances((vm_id,))
-      return True
-    except boto.exception.EC2ResponseError:
-      sleep(retry)
-  return False
- 
+create_class(students)
 
-public,private = create_local_keypair('ckanich')
-print public
-print private
+# wait_for_instance(boto.config.get_value('Custom','my_vm'))
 
 # start_student_vm()
 
