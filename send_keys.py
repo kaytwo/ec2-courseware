8f935210fa533abf7080a3761254b8c932af36bd
Chris Kanich <ckanich@uic.edu>
2016-01-13 18:44:05 -0600

    azure stuff added

--- send_keys.py
+++ send_keys.py
@@ -51,13 +51,10 @@ def send_mail(send_from, send_to, send_cc, subject, text, files={}, server="bcud
 
 
 # class specific variables
-CLASS = 'cs450'
-SEMESTER = 'f15'
+CLASS = 'cs361'
+SEMESTER = 's16'
 SERVER = 'git.uicbits.net'
 GITOLITE_USER = 'git'
-# AMI = 'ami-d0f89fb9' # for now, standard ubuntu 12.04.2 LTS
-AMI = 'ami-864d84ee' # ubuntu 14.04 lts (hvm)
-ACCOUNT = '020404094600' # ckanich
 CC_ADDR = 'Chris Kanich <ckanich@uic.edu>'
 REPLYTO = CC_ADDR
 
@@ -75,7 +72,7 @@ by group or other.
 
 Using your private key, everyone in class has read access to the public repository. If you would like to check the code out by itself, you can do so at:
 
-git clone {user}@{server}:{class}-{semester}/public.git
+git clone {user}@{server}:{classname}-{semester}/public.git
 
 This will make a subdirectory called "public" with the current version of the all published skeleton code for the class.
 
@@ -85,7 +82,12 @@ You can also checkout your personal repository like so:
 
 git clone {user}@{server}:{classname}-{semester}/{netid}.git
 
-Only you and the course admins can make updates to that repository. You will turn in your code by pushing updates to this repository.
+Only you and the course admins can read or make updates to that repository. You will turn in your code by pushing updates to this repository.
+
+
+You can also use the Microsoft Azure cloud platform to deploy your class virtual machine. You can redeem this code at http://www.microsoftazurepass.com/
+
+{azure_code}
 
 '''
 
@@ -104,16 +106,22 @@ def create_local_keypair(username):
 
 class Student:
   def __init__(self,csvline):
-    self.email = csvline
+    # must be in format from my bookmarklet
+    # that format is:
+    # lastname; firstname, uin, registration status, email address
+    name, uin, status, email = csvline.split(',')
+    lastname, firstname = name.split(';')
+    self.name = firstname.strip() + " " + lastname.strip()
+    self.email = email
     self.username = self.email.split('@')[0]
 
   def emailto(self):
-    return "%s <%s>" % (self.username, self.email)
+    return "%s <%s>" % (self.name, self.email)
 
   def send_mail(self):
     assert hasattr(self,'private_key')
     files = {'id_rsa':self.private_key}
-    send_mail(SEND_FROM_ADDR,self.emailto(),CC_ADDR,"Your %s VM credentials" % CLASS,email_text.format(user=GITOLITE_USER,server=SERVER,classname=CLASS,semester=SEMESTER,netid=self.username),files)
+    send_mail(SEND_FROM_ADDR,self.emailto(),CC_ADDR,"Your %s ssh credentials" % CLASS,email_text.format(user=GITOLITE_USER,server=SERVER,classname=CLASS,semester=SEMESTER,netid=self.username,azure_code=self.azure_code),files)
 
 
 def create_key_pair(username):
@@ -121,7 +129,7 @@ def create_key_pair(username):
   return pubkey,privkey
 
 
-def create_class(classlist):
+def create_class(classlist,codes):
   keydir = "%s-%s" % (CLASS,SEMESTER)
   try:
     os.mkdir(keydir)
@@ -132,6 +140,7 @@ def create_class(classlist):
   for student in classlist:
     student_id = '%s.%s.%s' % (CLASS,SEMESTER,student.username)
     student.student_id = student_id
+    student.azure_code = codes.pop()
     # create student ssh keypair
     public_key,private_key = create_key_pair(student_id)
     student.public_key = public_key
@@ -148,20 +157,44 @@ def read_student_file(filename):
   students = []
   with open(filename) as f:
     for line in f:
-      students.append(Student(line.strip()))
+      try:
+        students.append(Student(line.strip()))
+      except IndexError:
+        pass
+
   return students
+
+def read_azure_file(filename):
+  codes = []
+  with open(filename) as f:
+    for line in f:
+      tokens = line.strip().split()
+      # assuming azure codes are always 14 chars long
+      if len(tokens) == 1 and len(tokens[0]) == 14:
+        codes.append(tokens[0])
+  
+  return codes
+
+def dump_azure_file(filename, codes):
+  with open(filename,'w') as f:
+    for k in codes:
+      f.write("%s\n" % k)
+
+
+
+
+
 
 # students = read_student_file('cs450.csv')
 students = read_student_file(sys.argv[1])
 
+codes = read_azure_file(sys.argv[2])
+
 # create_access_keys(students)
 
-create_class(students)
+# this method removes entries from the codes list
+create_class(students, codes)
+
+dump_azure_file(sys.argv[2],codes)
 
 
-
-# wait_for_instance(boto.config.get_value('Custom','my_vm'))
-
-# start_student_vm()
-
-# create_class(['ckanich-testing',])
